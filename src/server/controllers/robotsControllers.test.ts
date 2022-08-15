194cc2c99056710bc2ccd69d447074ffb9326544
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-15 21:32:54 +0200

    added createRobots controller, error and custom error

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Robot from "../../database/models/Robot";
-import getRobots from "./robotsControllers";
+import { getRobots } from "./robotsControllers";
 
 describe("Given a robotsController middleware", () => {
   describe("When it receives a response object", () => {
     test("Then it should call the response mathod with status 200", async () => {
       const req = {} as Partial<Request>;
+      const next = {} as NextFunction;
       const res = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
@@ -13,7 +14,11 @@ describe("Given a robotsController middleware", () => {
       Robot.find = jest.fn();
       const status = 200;
 
-      await getRobots(req as Request, res as unknown as Response);
+      await getRobots(
+        req as Request,
+        res as unknown as Response,
+        next as NextFunction
+      );
 
       expect(res.status).toHaveBeenCalledWith(status);
     });
