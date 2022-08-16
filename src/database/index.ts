34e1ffca049d6734e7d44f93c7c80ff1a44a833a
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-16 19:25:41 +0200

    added set to connect DB for replace id and v

--- src/database/index.ts
+++ src/database/index.ts
@@ -15,6 +15,17 @@ const connectDataBase = (mongoUrl: string) =>
       }
       debug(chalk.greenBright("Connected with Database"));
       resolve(true);
+      mongoose.set("toJSON", {
+        virtuals: true,
+        transform: (doc, ret) => {
+          const newDocument = { ...ret };
+          // eslint-disable-next-line no-underscore-dangle
+          delete newDocument._v;
+          // eslint-disable-next-line no-underscore-dangle
+          delete newDocument._id;
+          return newDocument;
+        },
+      });
     });
   });
 
