7e3a04fc176c6db168e4f24cbd97a4e73b0bf2c3
Fernando Paulino <xizes2@hotmail.com>
2022-08-16 21:11:43 +0200

    include file extension

--- src/server/middlewares/authentication.ts
+++ src/server/middlewares/authentication.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import { verifyToken } from "../../utils/auth";
-import CreateCustomError from "../../utils/CreateCustomError";
+import CreateCustomError from "../../utils/CreateCustomError.ts";
 import CustomError from "../types/error";
 
 interface ICustomRequest extends Request {
